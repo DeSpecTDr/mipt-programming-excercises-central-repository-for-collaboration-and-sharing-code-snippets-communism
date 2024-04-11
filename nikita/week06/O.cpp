7eb5f3eaa528ce46d91ba287e56d4fe61ff13c99
Riahovsky <riakhovskii.ns@phystech.edu>
2024-04-11 15:17:26 +0300

    chore: inital commit

---
+++ nikita/week06/O.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+//#ifndef N
+//#define N 100
+//#endif
+struct Student {
+    std::string name;
+    int mark;
+};
+
+int main() { 
+    Student F[10];
+    Student M[10];
+    M[9].name = "f";
+    
+    return 0;
+}
